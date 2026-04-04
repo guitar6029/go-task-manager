837569c8ec996e04f8b926ec4af4ee44968e1f09
Josh <jsbusiness305@gmail.com>
2026-04-04 11:04:57 -0400

    add params for tasks GET request

--- api/task_handler.go
+++ api/task_handler.go
@@ -3,6 +3,7 @@ package api
 import (
 	"database/sql"
 	"net/http"
+	"strconv"
 	servicepkg "taskmanager/service"
 
 	"github.com/gin-gonic/gin"
@@ -10,7 +11,32 @@ import (
 
 func GetTasksHandler(db *sql.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tasks, err := servicepkg.GetTasks(db, "", 5, 0)
+		//read query params
+		limitStr := c.Query("limit")
+		offsetStr := c.Query("offset")
+		doneStr := c.Query("done")
+
+		// convert
+		limit := 5
+		offset := 0
+		filter := ""
+
+		if l, err := strconv.Atoi(limitStr); err == nil && l > 0 {
+			limit = l
+		}
+
+		if o, err := strconv.Atoi(offsetStr); err == nil && o >= 0 {
+			offset = o
+		}
+
+		switch doneStr {
+		case "true":
+			filter = "done"
+		case "false":
+			filter = "pending"
+		}
+
+		tasks, err := servicepkg.GetTasks(db, filter, limit, offset)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 			return
