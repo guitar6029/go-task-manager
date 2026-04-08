c224f255237e9f9d0f8361997b2e6e4aabd80e68
Josh <jsbusiness305@gmail.com>
2026-04-08 12:31:58 -0400

    add exclusion to swagger to nginx conf

--- middleware/rate-limiter.go
+++ middleware/rate-limiter.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/time/rate"
@@ -11,6 +12,13 @@ var limiter = rate.NewLimiter(2, 5)
 
 func RateLimiter() gin.HandlerFunc {
 	return func(c *gin.Context) {
+
+		//swagger exclude
+		if strings.HasPrefix(c.Request.URL.Path, "/swagger") {
+			c.Next()
+			return
+		}
+
 		if !limiter.Allow() {
 			c.JSON(http.StatusTooManyRequests, gin.H{
 				"error": "too many requests",
